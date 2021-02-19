40da79d3471e4378f5b561be477d4777243fff4e
DongJae Seo <75518803+dongjaeseo@users.noreply.github.com>
2021-02-20 01:02:25 +0900

    오늘 안에 진짜 끝낸다!!

--- check.py
+++ check.py
@@ -42,15 +42,8 @@ images_path = '../input/flickr8k/Data/Flicker8k_Dataset/'
 glove_path = '../input/glove6b'
 
 doc = open(token_path,'r').read()
-# print(doc[:410])
 
-# 캡션+이미지 사진을 줄별로 잘라준다
-# len(line) > 2 는 마지막에 빈 줄때문에 있다
-# token = line.split() 하면 줄이 다 나눠진다
-# image_id 에 tokens[0] 를 .jpg 빼주고
-# 띄워놓은 image_desc 를 다시 붙여줌 스페이스로
 # 그리고 딕셔너리 형태로 이미지이름:[캡션1,캡션2] 형태로 저장
-# '1034276567_49bb87c51c': ['a boy bites hard into a treat while he sits outside '
 descriptions = dict()
 table = str.maketrans('', '', string.punctuation)
 for line in doc.split('\n'):
@@ -66,6 +59,7 @@ for line in doc.split('\n'):
             if image_id not in descriptions:
                 descriptions[image_id] = list()
             descriptions[image_id].append(image_desc)
+# '1034276567_49bb87c51c': ['a boy bites hard into a treat while he sits outside '
 
 # 트레인 테스트 스플릿
 image_id = []
@@ -88,38 +82,68 @@ for id in val_id:
 # 시퀀스 시작과 끝 찍어준다
 train_descriptions = {}
 for id, caption in descriptions.items():
-    if id not in train_descriptions:
-        train_descriptions[id] = []
-    for i in range(len(caption)):
-        tokens = caption[i].split()
-        new_caption = 'startseq ' + ' '.join(tokens) + ' endseq'
-        train_descriptions[id].append(new_caption)
+    if id in train_id:
+        if id not in train_descriptions:
+            train_descriptions[id] = []
+        for i in range(len(caption)):
+            tokens = caption[i].split()
+            new_caption = 'startseq ' + ' '.join(tokens) + ' endseq'
+            train_descriptions[id].append(new_caption)
 
 val_descriptions = {}
 for id, caption in descriptions.items():
-    if id not in val_descriptions:
-        val_descriptions[id] = []
-    for i in range(len(caption)):
-        tokens = caption[i].split()
-        new_caption = 'startseq ' + ' '.join(tokens) + ' endseq'
-        val_descriptions[id].append(new_caption)
-
+    if id in val_id:
+        if id not in val_descriptions:
+            val_descriptions[id] = []
+        for i in range(len(caption)):
+            tokens = caption[i].split()
+            new_caption = 'startseq ' + ' '.join(tokens) + ' endseq'
+            val_descriptions[id].append(new_caption)
 # print(train_descriptions) '1032460886_4a598ed535': ['startseq a man is standing in front of a skyscraper endseq',
 # print(val_descriptions) '1032460886_4a598ed535': ['startseq a man is standing in front of a skyscraper endseq'
 
-all_train_captions = []
-for key, val in train_descriptions.items():
-    for cap in val:
-        all_train_captions.append(cap)
+# 반복되는 단어 찾기
+all_captions = []
+for _, captions in train_descriptions.items():
+    for line in captions:
+        all_captions.append(line)
+        
+for _, captions in val_descriptions.items():
+    for line in captions:
+        all_captions.append(line)
+# print(len(all_captions), all_captions[0:10]) 40460 ['startseq a child in a pink dress is climbing up a set of stairs in an entry way endseq'
 
-# 그래도 10번은 반복되어야 의미있는 단어 아니겠소?
-# 10번도 반복안되면 쓰지 말자!!! 한번 숫자 바꿔보자!!
-# 딕셔너리 워드마다 숫자 늘어남!!
-word_count_threshold = 10
+word_repeat = 10
 word_counts = {}
-nsents = 0
-for sent in all_train_captions:
-    nsents += 1
-    for w in sent.split(' '):
-        word_counts[w] = word_counts.get(w, 0) + 1
-vocab = [w for w in word_counts if word_counts[w] >= word_count_threshold]
+for line in all_captions:
+    for w in line.split(' '):
+        word_counts[w] = word_counts.get(w, 0) + 1 
+
+vocab = [word for word in word_counts if word_counts[word] >= word_repeat]
+# print(len(vocab)) 1957
+
+# wordtoix 는 {단어:숫자}
+# ixtoword 는 {숫자:단어}
+# 1부터 숫자 부여해줌
+int_word = {}
+word_int = {}
+number = 1
+for word in vocab:
+    int_word[number] = word
+    word_int[word] = number
+    number += 1
+# print(int_word) 1010: 'museum'
+# zero padding 때문
+vocab_size = len(int_word) + 1
+
+# 전체문장 중 제일 단어 수가 많은 문장의 단어 수 찾기!
+num = []
+for _, captions in train_descriptions.items():
+    for line in captions:
+        num.append(len(line.split()))
+for _, captions in val_descriptions.items():
+    for line in captions:
+        num.append(len(line.split()))
+
+max_length = np.max(num)
+print(max_length)
